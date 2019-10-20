439ed16d36a69db2a2027b0e578c8bdf7254ff01
minsookim <minsookim0615@gmail.com>
2019-10-20 15:35:41 +0900

    Initial Commit

---
+++ eightDays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eightDays
+//
+//  Created by minsoo kim on 2019/10/20.
+//  Copyright © 2019 minsoo kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
