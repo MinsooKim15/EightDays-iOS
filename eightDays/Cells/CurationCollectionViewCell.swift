21e274b91a461d3aadcc2f4c8e0a300b2b0c4691
minsookim <minsookim0615@gmail.com>
2020-03-09 11:06:49 +0900

    #5 * Make top corners of curation cell round

--- eightDays/Cells/CurationCollectionViewCell.swift
+++ eightDays/Cells/CurationCollectionViewCell.swift
@@ -41,8 +41,9 @@ class CurationCollectionViewCell: UICollectionViewCell {
                 }
             }
                 curationImageView.contentMode = .scaleAspectFill
-            //        curationImageView.clipsToBounds = true
+                    curationImageView.clipsToBounds = true
                     curationImageView.layer.masksToBounds = true
+            curationImageView.roundCorners([.topLeft, .topRight], radius: 10)
         }
     }
 //    func populate(smallPlace: SmallPlace){
@@ -56,3 +57,13 @@ class CurationCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var score: UILabel!
 }
 
+extension UIView {
+
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+         let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+         let mask = CAShapeLayer()
+         mask.path = path.cgPath
+         self.layer.mask = mask
+    }
+
+}
